e7e306c55f5420953a64f4b433f6389140e3e0b8
AliyevaMadina <mdn.alyv@gmail.com>
2024-11-17 20:24:51 +0400

    6

--- ES6_classes/6-sky_high.js
+++ ES6_classes/6-sky_high.js
@@ -1,10 +1,10 @@
-import Building from './5-building.js';
+import Building from './5-building';
 
 export default class SkyHighBuilding extends Building {
   constructor(sqft, floors) {
     super(sqft);
     this._floors = floors;
-    }
+ }
 
   get sqft() {
     return this._sqft;
