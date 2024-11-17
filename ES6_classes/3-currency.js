be383a464280bc707f868800a505b6091f6ffb57
AliyevaMadina <mdn.alyv@gmail.com>
2024-11-17 19:28:17 +0400

    3

--- ES6_classes/3-currency.js
+++ ES6_classes/3-currency.js
@@ -1,13 +1,6 @@
 export default class Currency {
   constructor(code, name) {
-    if (typeof code !== 'string') {
-      throw new TypeError('Code must be a string');
-    }
     this._code = code;
-
-    if (typeof name !== 'string') {
-      throw new TypeError('Name must be a string');
-    }
     this._name = name;
   }
 
@@ -15,22 +8,8 @@ export default class Currency {
     return this._code;
   }
 
-  set code(value) {
-    if (typeof value !== 'string') {
-      throw new TypeError('Code must be a string');
-    }
-    this._code = value;
-  }
-
   get name() {
     return this._name;
-  }
-
-  set name(value) {
-    if (typeof value !== 'string') {
-      throw new TypeError('Name must be a string');
-    }
-    this._name = value;
   }
 
   displayFullCurrency() {
