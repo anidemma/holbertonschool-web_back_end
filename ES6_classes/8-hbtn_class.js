38ffe4967c73c7c3d5f46099772794aca832044d
AliyevaMadina <mdn.alyv@gmail.com>
2024-11-17 20:39:38 +0400

    8

---
+++ ES6_classes/8-hbtn_class.js
@@ -0,0 +1,21 @@
+class HolbertonClass {
+  constructor(size, location) {
+    if (typeof size !== 'number') {
+      throw new TypeError('Size must be a number');
+    }
+    this._size = size;
+
+    if (typeof location !== 'string') {
+      throw new TypeError('Location must be a string');
+    }
+    this._location = location;
+  }
+
+  toString() {
+    return `${this._location}`;
+  }
+
+  valueOf() {
+    return `${this._size}`;
+  }
+}
