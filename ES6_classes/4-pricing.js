853e3c18da5138ab708e4755d20f5a97bfe5aa41
AliyevaMadina <mdn.alyv@gmail.com>
2024-11-17 19:33:51 +0400

    4

--- ES6_classes/4-pricing.js
+++ ES6_classes/4-pricing.js
@@ -3,10 +3,10 @@
 import Currency from './3-currency.js';
 
 export default class Pricing {
-    constructor(amount, currency) {
-      this._amount = amount;
-      this._currency = currency;
-    }
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
 
   get amount() {
     return this._amount;
