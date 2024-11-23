31a21c5e81ee3cb3a3a31f61d7a60213b45ab98f
AliyevaMadina <mdn.alyv@gmail.com>
2024-11-23 17:05:10 +0400

    1 fixed

--- ES6_data_manipulation/1-get_list_student_ids.js
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,6 +1,6 @@
 export default function getListStudentIds(getListStudents) {
-  if(Array.isArray(getListStudents)) {
-    return [getListStudents.map(student => student.id)];
+  if(!Array.isArray(getListStudents)) {
+    return [];
   }
-  return [];
+  return (getListStudents.map(student => student.id));
 }
