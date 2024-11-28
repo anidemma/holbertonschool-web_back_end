7ad3282f599a03b5265d8f52c337369c75d5bd22
AliyevaMadina <mdn.alyv@gmail.com>
2024-11-28 19:27:42 +0400

    4

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,10 +1,9 @@
 export default function updateStudentGradeByCity(getListStudents, city, newGrades) {
   const grd = getListStudents.filter((student) => student.location === city);
 
-  grd.map((student) => {
+  return grd.map((student) => {
     const gradeEntry = newGrades.find((grade) => grade.studentId === student.id);
-    student.grade = gradeEntry ? gradeEntry.grade : 'N/A';
+    return { ...student, grade: gradeEntry ? gradeEntry.grade : 'N/A' };
   });
-
-  return grd;
 }
+  
