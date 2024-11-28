e582306aefc38f64d368006895d831bea089e45b
AliyevaMadina <mdn.alyv@gmail.com>
2024-11-28 19:34:08 +0400

    5

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -1,11 +1,10 @@
 export default function createInt8TypedArray(length, position, value) {
-    const buff = new ArrayBuffer(length);
-    const int8arr = new Int8Array(buff);
-    if (position > length) {
-      throw Error('Position outside range');
-    } else {
-      int8arr[position] = value;
-    }
-    return new DataView(buff);
+  const buff = new ArrayBuffer(length);
+  const int8arr = new Int8Array(buff);
+  if (position > length) {
+    throw Error('Position outside range');
+  } else {
+    int8arr[position] = value;
   }
-  
+  return new DataView(buff);
+}
