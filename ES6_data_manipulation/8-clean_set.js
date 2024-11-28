96edb6b6cdcd677846ca863f80fa4257766c1ede
AliyevaMadina <mdn.alyv@gmail.com>
2024-11-28 19:40:47 +0400

    8

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,16 +1,15 @@
 export default function cleanSet(set, startString) {
-
   if (typeof startString !== 'string' || startString.length === 0) {
     return '';
   }
 
-  const full_string = [];
+  const fullString = [];
 
   for (const str of set) {
     if (str.startsWith(startString)) {
-        full_string.push(str.slice(startString.length));
+      fullString.push(str.slice(startString.length));
     }
   }
 
-  return full_string.join('-');
+  return fullString.join('-');
 }
